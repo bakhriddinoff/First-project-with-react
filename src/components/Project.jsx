f90d7b4af5d710ba5a0a1aab7db0efbd4009e4d1
Ozodbek <bakhriddinoff01@gmail.com>
2025-08-17 10:52:42 +0500

    added Project section

---
+++ src/components/Project.jsx
@@ -0,0 +1,25 @@
+import './Project.css'
+import code from '../assets/img.png'
+
+function Project() {
+    return (
+        <div className="section1">
+            <h2 className='heading1'>Select project, <br/> start coding</h2>
+            <div className="wrapper">
+                <div className="left">
+                    <h3 className='headinger1'>Think CI/CD for dev environments</h3>
+                    <p className="text1">We invented prebuilds so application code, configuration and <br /> infrastructure can all be stored as machine-executable code <br /> in your git repositories and applied to dev environments <br /> automatically and continuously.</p>
+                    <div className="btn-wrapper">
+                        <button className="btn2">More on prebuilds</button>
+                        <button className="btn2 btn3">Documentation</button>
+                    </div>
+                </div>
+                <div className="right">
+                    <img className='img1' src={code} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
